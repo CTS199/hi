46a5ff0a6a18fd4e828e475acd04ded171de7892
Hans Han <han13066022700@gmail.com>
2022-08-01 09:38:54 +0800

    second commit

--- hi.py
+++ hi.py
@@ -1,2 +1,29 @@
-name = input('请输入名字：')
-print('Hey'	, name)
+# name = input('请输入名字：')
+# print('Hey'	, name)
+# 哈哈哈
+# print('who am i')
+
+# x = 5
+# print (x == 5)
+# print (x != 2)
+# print (x <= 5)
+
+#if类型搭配else
+# rain = input ('Will rain today? (pls type yes/no):')
+# if rain == 'yes':
+# 	print('You are fking smart')
+# else:
+# 	print('You foolish')
+
+#判断
+# age = input('Put your age:')
+# age = int(age)
+# if age >= 20:
+# 	print('You can vote!!!')
+
+#转换
+C = input('Please typr the Temp:') #输入类型String
+C = float(C) #String 转换float
+F = 1.8 * C + 32 #此时F是float
+# F = float(F)
+print ('US Temp is', F)
